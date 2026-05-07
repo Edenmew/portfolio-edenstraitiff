fdb892946d23e2b6ecbef363d77f36b6bb79a5b4
estra <estraitiff@gmail.com>
2026-05-06 21:11:46 -0700

    format json properly, again, start on loadCards.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 
-const jsonData = {
-    "Projects": [{"Project Name": "RGB Light Model in C, Graphics",
+const project_cards =
+    [ {"Project Name": "RGB Light Model in C, Graphics",
     "Technologies Used": "C, FPToolkit",
     "Description": "sample",
     "Image": "sample",
@@ -19,8 +19,7 @@ const jsonData = {
     "Technologies Used": "sample",
     "Description": "sample",
     "Image": "sample",
-    "Link": "sample"}
-]}
+    "Link": "sample"} ]
 
 data = JSON.stringify(jsonData)
 
@@ -33,6 +32,9 @@ function domLoaded()
 {
     const funBtn = document.getElementById("fun");
     funBtn.addEventListener("click", changeColors);
+
+    loadCards();
+
 }
 
 function changeColors()
@@ -40,18 +42,23 @@ function changeColors()
     const root = document.documentElement;
     if (color_toggle == 0)
     {
-        root.style.setProperty('--outer_background_color', 'black')
-        root.style.setProperty('--inner_background_color', '#101010')
-        root.style.setProperty('--text_color', 'red')
-        root.style.setProperty('--hover_color', '#151515')
+        root.style.setProperty('--outer_background_color', 'black');
+        root.style.setProperty('--inner_background_color', '#101010');
+        root.style.setProperty('--text_color', 'red');
+        root.style.setProperty('--hover_color', '#151515');
         color_toggle = 1;
     }
     else
     {
-        root.style.setProperty('--outer_background_color', '#022C04')
-        root.style.setProperty('--inner_background_color', '#124012')
-        root.style.setProperty('--text_color', '#DCA028')
-        root.style.setProperty('--hover_color', '#044E08')
+        root.style.setProperty('--outer_background_color', '#022C04');
+        root.style.setProperty('--inner_background_color', '#124012');
+        root.style.setProperty('--text_color', '#DCA028');
+        root.style.setProperty('--hover_color', '#044E08');
         color_toggle = 0;
     }
+}
+
+function loadCards()
+{
+
 }

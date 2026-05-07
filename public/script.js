07cbbf11b5b5d8fa03ef8aee27be4f3cbecd7ec8
estra <estraitiff@gmail.com>
2026-05-06 20:52:23 -0700

    Color toggle!

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-const jsonData = [
+/*
+const jsonData = {[
     {"Project Name": "RGB Light Model in C, Graphics",
     "Technologies Used": "C, FPToolkit",
     "Description": "sample",
@@ -18,5 +19,38 @@ const jsonData = [
     "Technologies Used": "sample",
     "Description": "sample",
     "Image": "sample",
-    "Link": "sample" }];
-data = JSON.parse(jsonData)
+    "Link": "sample" }]};
+    */
+
+//data = JSON.parse(jsonData)
+
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+color_toggle = 0;
+
+function domLoaded()
+{
+    const funBtn = document.getElementById("fun");
+    funBtn.addEventListener("click", changeColors);
+}
+
+function changeColors()
+{
+    const root = document.documentElement;
+    if (color_toggle == 0)
+    {
+        root.style.setProperty('--outer_background_color', 'black')
+        root.style.setProperty('--inner_background_color', '#101010')
+        root.style.setProperty('--text_color', 'red')
+        root.style.setProperty('--hover_color', '#151515')
+        color_toggle = 1;
+    }
+    else
+    {
+        root.style.setProperty('--outer_background_color', '#022C04')
+        root.style.setProperty('--inner_background_color', '#124012')
+        root.style.setProperty('--text_color', '#DCA028')
+        root.style.setProperty('--hover_color', '#044E08')
+        color_toggle = 0;
+    }
+}
